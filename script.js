cf8887194739317e0f144e1adc0f6e77df89c93c
sophia771997 <sophia.sophia771997@gmail.com>
2026-06-28 13:03:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
+const chatBox = document.getElementById("chatBox");
+const userInput = document.getElementById("userInput");
+const sendBtn = document.getElementById("sendBtn");
 
+function addMessage(text, sender) {
+
+    const div = document.createElement("div");
+
+    div.className = sender;
+
+    div.innerHTML = text;
+
+    chatBox.appendChild(div);
+
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
+
+function botReply(userMessage){
+
+    setTimeout(()=>{
+
+        addMessage("🤖 Demo Reply:<br><br>You said:<br><b>"+userMessage+"</b><br><br>Next we'll connect Gemini AI.", "bot");
+
+    },700);
+
+}
+
+sendBtn.addEventListener("click", ()=>{
+
+    const msg = userInput.value.trim();
+
+    if(msg==="") return;
+
+    addMessage(msg,"user");
+
+    userInput.value="";
+
+    botReply(msg);
+
+});
+
+userInput.addEventListener("keypress",function(e){
+
+    if(e.key==="Enter"){
+
+        sendBtn.click();
+
+    }
+
+});
